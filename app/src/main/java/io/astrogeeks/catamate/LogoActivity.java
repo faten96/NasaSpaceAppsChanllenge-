9155ddb0eb1a72e69fca3cd6a8422a448513d741
RahimACM <rahimhaneche@gmail.com>
2018-10-20 10:44:06 +0100

    forgotten new files to commit

---
+++ app/src/main/java/io/astrogeeks/catamate/LogoActivity.java
@@ -0,0 +1,30 @@
+
+/*
+ * Copyright (c) 2018. Tous les droits sont réservés.
+ */
+
+package io.astrogeeks.catamate;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+
+        int splashTime = 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                Intent mainIntent = new Intent(LogoActivity.this, MainActivity.class);
+//                startActivity(mainIntent);
+//                finish();
+            }
+        }, splashTime);
+    }
+}
