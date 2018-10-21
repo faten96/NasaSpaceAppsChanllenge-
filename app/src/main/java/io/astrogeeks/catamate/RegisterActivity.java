e23f82791841d4779781d1e1b76e5616f668534a
RahimACM <rahimhaneche@gmail.com>
2018-10-21 05:37:19 +0100

    InfosActivity done

--- app/src/main/java/io/astrogeeks/catamate/RegisterActivity.java
+++ app/src/main/java/io/astrogeeks/catamate/RegisterActivity.java
@@ -34,14 +34,6 @@ public class RegisterActivity extends AppCompatActivity implements LoaderManager
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-
     private AutoCompleteTextView mUsernameView;
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
