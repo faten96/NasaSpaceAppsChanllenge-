ac76c265dac3c54b475ce18e9c24b40ea22ece3a
RahimACM <rahimhaneche@gmail.com>
2018-10-24 16:10:03 +0100

    add not completed HomeActivity

--- app/src/main/java/io/astrogeeks/catamate/InfosActivity.java
+++ app/src/main/java/io/astrogeeks/catamate/InfosActivity.java
@@ -1,5 +1,6 @@
 package io.astrogeeks.catamate;
 
+import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +12,26 @@ public class InfosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos);
+
+        findViewById(R.id.save_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validateInput()){
+                    saveInfos();
+                    Intent homeIntent = new Intent(InfosActivity.this, HomeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }
+        });
     }
+
+    private boolean validateInput(){
+        // TODO fix it
+        return true;
+    }
+
+    private void saveInfos(){}
 
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
